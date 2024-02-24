d3502b159e9c9bf3288c5090b1e96151667bcda6
Robert C <cmrecki.robert@gmail.com>
2024-02-24 22:38:45 +0100

    Finished CRUD operations with querries

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,7 +4,6 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
-import { Repository } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 @Injectable()
@@ -14,31 +13,10 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  /*
-  getAllTasks(): Task[] {
-    return this.tasks;
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
   }
 
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
-
-    if (search) {
-      tasks = tasks.filter(
-        (task) =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-
-    return tasks;
-  }
-*/
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
 
@@ -61,22 +39,17 @@ export class TasksService {
     }
   }
 
-  /*
-
-async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = this.getTaskById(id);
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
     task.status = status;
+    await task.save();
     return task;
   }
+
+  /*
 
   deleteAllTasks(): void {
     this.tasks = [];
-  }
-
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    const task = this.getTaskById(id);
-    task.status = status;
-    return task;
   }
   */
 }
