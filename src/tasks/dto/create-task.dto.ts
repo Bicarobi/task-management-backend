a8eb8a275a980ef775588b3c622b93bee853e7b4
Robert C <cmrecki.robert@gmail.com>
2024-03-30 16:12:01 +0100

    Added status as option

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
+import { TaskStatus } from '../task-status.enum';
 
 export class CreateTaskDto {
   @IsNotEmpty()
@@ -6,4 +7,8 @@ export class CreateTaskDto {
 
   @IsNotEmpty()
   description: string;
+
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+  status: TaskStatus;
 }
