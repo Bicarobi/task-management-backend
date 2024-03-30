e344e502ce6641dd0b0839901fb1818d75626ea8
Robert C <cmrecki.robert@gmail.com>
2024-03-30 12:38:34 +0100

    Reverted import changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { MulterModule } from '@nestjs/platform-express';
-import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule,
-    AuthModule,
-    MulterModule.register({ dest: './uploads' }),
-    FilesModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
 })
 export class AppModule {}
